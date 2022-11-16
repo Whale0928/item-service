8ffda0c40937e1f33b00148c4cfc520dc4194f17
whale0928 <rlagusrl928@gmail.com>
2022-11-16 19:50:18 +0900

    feat : Repository Create
    - Create Repository class
    - implement method
    - find one
    - find All
    - update Item Object

--- src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -2,7 +2,9 @@ package hello.itemservice.domain.item;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -19,6 +21,39 @@ public class ItemRepository {
         item.setId(++sequence);  //저장 전 시퀀스 증가
         store.put(item.getId(),item); // 여기서 시퀀스를 저장 시키면 다른 사용자가 접근할떄 중복 증가 하는 경우가 발생
         return item;
+    }
+
+    /**아이디로 아이템 찾기
+     * Find by id item.
+     *
+     * @param id the id
+     * @return the item
+     */
+    public Item findById(Long id){
+        return store.get(id);
+    }
+
+    /**맵을 리스트로 한번 감싸 전체 조회하는 메서드
+     * Find all list.
+     *
+     * @return the list
+     */
+    public List<Item> findAll(){
+        return new ArrayList<>(store.values());
+    }
+
+    /**업데이트 기능
+     * Update.
+     *
+     * @param id          the id
+     * @param updateParam the update param
+     */
+    public void update(Long id , Item updateParam){
+        Item findItem = findById(id);
+        //프로젝트 규모에 따라 업데이트용 dto를 만드는 것이 바람직하다.
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
     }
 }
 
