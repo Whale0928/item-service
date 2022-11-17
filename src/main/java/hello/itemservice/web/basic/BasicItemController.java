3c942466a1c9ace868b4c028188a55325fb21661
whale0928 <rlagusrl928@gmail.com>
2022-11-17 20:28:03 +0900

    feat :  Implement All Controller
    - Select List
    - Select One By Id
    - Update Item
    - Regi Item

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -3,11 +3,10 @@ package hello.itemservice.web.basic;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -19,18 +18,83 @@ public class BasicItemController {
     //@RequiredArgsConstructor 으로 인해 생성자가 없어도 자동 주입
     private final ItemRepository itemRepository;
 
-    @GetMapping
-    public String items(Model modle){
-        List<Item> items = itemRepository.findAll();
-        modle.addAttribute("items",items);
-        return "basic/items";
-    }
-
-    /**테스트 아이템
+    /**
+     * 테스트 아이템
      * Init.
      */
     @PostConstruct
-    public void init(){
-        itemRepository.save(new Item("itemName",10000,10));
+    public void init() {
+        itemRepository.save(new Item("itemNameOne", 15000, 11));
+    }
+
+    /* 상품 전체 보기.
+     * Items string.
+     *
+     * @param modle the modle
+     * @return the string
+     */
+    @GetMapping
+    public String items(Model modle) {
+        List<Item> items = itemRepository.findAll();
+        modle.addAttribute("items", items);
+        return "basic/items";
+    }
+
+
+    /**
+     * 상품 상세보기
+     * Detail string.
+     *
+     * @param itemId the item id
+     * @param model  the model
+     * @return the string
+     */
+    @GetMapping("/{itemId}")
+    public String detail(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    /**
+     * 신규 아이템 화면
+     * Add string.
+     *
+     * @return the string
+     */
+    @GetMapping("/add")
+    public String add() {
+        return "basic/addForm";
+    }
+
+    /**
+     * 신규 아이템 저장
+     * Add string.
+     *
+     * @return the string
+     */
+    @PostMapping("/add")
+    public String save(Model model, @ModelAttribute Item item) {
+        Item saveItem = itemRepository.save(item);
+        model.addAttribute("item", saveItem);
+        return "basic/item";
+    }
+
+    @GetMapping("{itemId}/edit")
+    public String edit(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    @PostMapping("{itemId}/edit")
+    public String edit(@PathVariable Long itemId
+            , @ModelAttribute Item newItem
+            , Model model) {
+        itemRepository.update(itemId, newItem);
+        Item updateItem = itemRepository.findById(itemId);
+        model.addAttribute("item", updateItem);
+    return "basic/item";
     }
 }
